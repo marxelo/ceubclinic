958518434bad6a166e050300100effd9d7ceac5d
Marcelo Maciel <marselo.maciel@gmail.com>
2024-04-08 09:15:15 -0300

    initial commit

---
+++ src/main/java/com/pwd/ceubclinic/CeubclinicApplication.java
@@ -0,0 +1,13 @@
+package com.pwd.ceubclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeubclinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeubclinicApplication.class, args);
+	}
+
+}
